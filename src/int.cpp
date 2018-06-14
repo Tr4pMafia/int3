8f6ce31b9582928ec92d1f8f9e1b8477f2753a93
morimolymoly <morimolymoly@gmail.com>
2018-06-15 01:04:08 +0900

    first commit

---
+++ src/int.cpp
@@ -0,0 +1,65 @@
+#include <bfvmm/hve/arch/intel_x64/exit_handler/exit_handler.h>
+#include <bfvmm/hve/arch/intel_x64/vcpu/vcpu.h>
+#include <bfvmm/vcpu/vcpu.h>
+#include <bfdebug.h>
+#include <bfvmm/vcpu/vcpu_factory.h>
+#include <bfgsl.h>
+#include <bfstring.h>
+#include <string>
+
+namespace mafia
+{
+namespace intel_x64
+{
+
+static bool handle_interrupt_window(gsl::not_null<bfvmm::intel_x64::vmcs *> vmcs)
+{
+    using namespace ::intel_x64::vmcs::idt_vectoring_information;
+    if(interruption_type::get() != interruption_type::software_interrupt) return true;
+    auto rax = vmcs->save_state()->rax;
+    std::string msg = "mafia"
+    dump(0, &msg);
+    return true;
+}
+
+class exit_handler_mafia : public bfvmm::intel_x64::exit_handler
+{
+public:
+    exit_handler_mafia(gsl::not_null<bfvmm::intel_x64::vmcs *> vmcs)
+    : exit_handler(vmcs)
+    {
+        using namespace ::intel_x64::vmcs;
+        bfdebug_info(0, "mafia hype you");
+        add_handler(
+            exit_reason::basic_exit_reason::interrupt_window,
+            handler_delegate_t::create<handle_interrupt_window>()
+        );
+    }
+};
+
+class mafia_vcpu : public bfvmm::intel_x64::vcpu
+{
+public:
+    mafia_vcpu(vcpuid::type id)
+    : vcpu(id)
+    {
+        m_exit_handler_mafia = std::make_unique<mafia::intel_x64::exit_handler_mafia>(vmcs());
+    }
+    mafia::intel_x64::exit_handler_mafia *exit_handler()
+    { return m_exit_handler_mafia.get(); }
+private:
+    std::unique_ptr<mafia::intel_x64::exit_handler_mafia> m_exit_handler_mafia;
+};
+}
+}
+
+namespace bfvmm
+{
+WEAK_SYM std::unique_ptr<bfvmm::vcpu>
+vcpu_factory::make_vcpu(vcpuid::type vcpuid, bfobject *obj)
+{
+    bfignored(obj);
+    return std::make_unique<mafia::intel_x64::mafia_vcpu>(vcpuid);
+}
+}
+
